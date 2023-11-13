fd057bbab8092f22a0740247484f6dd1441d211b
Damaipatria <damaipatriasuyana10@gmail.com>
2023-11-13 13:35:22 +0700

    tambah film bagus di portofolio

--- src/assets/data/Portofolio.js
+++ src/assets/data/Portofolio.js
@@ -3,11 +3,20 @@ const Portofolio = [
     id: 1,
     judul: 'Warung Makan Sepuasnya',
     teknologi: ['React', 'React-Bootstratp'],
-    deskripsi: 'Website ini merupakan sebuah warung makan yang ber tujuan untuk mempermudah konsumen dalam pemesanan. Pada website ini saya membuat website dengan menggunakan React JS dan menggunakan component dari React-Bootstrap.',
+    deskripsi: 'Website ini merupakan sebuah warung makan yang ber tujuan untuk mempermudah konsumen dalam pemesanan. Website ini saya buat dengan menggunakan React JS dan menggunakan component dari React-Bootstrap.',
     code: 'https://github.com/Damaipatria',
     demo: '/warung-makan/beranda',
     img: 'warung_makan_sepuasnya'
-  }
+  },
+  {
+    id: 2,
+    judul: 'Film Bagus',
+    teknologi: ['React', 'Tailwindcss', 'Vite'],
+    deskripsi: 'Website ini menampilkan film - film dan series yang terinspirasi dengan website streaming platform seperti Nefflix. Website ini saya buat dengan menggunakan React JS serta Tailwindcss dan menggunakan Vite.',
+    code: '',
+    demo: 'https://film-bagus.vercel.app/',
+    img: 'film-bagus'
+  },
 ]
 
 export default Portofolio;
